47d228407647c064758597ff48ef545ec0bdd466
AndyMB94 <andy.mallcco@tecsup.edu.pe>
2025-02-17 15:21:56 -0500

    mejore los permisos para staff en permissions

--- api/permissions.py
+++ api/permissions.py
@@ -2,42 +2,51 @@ from rest_framework import permissions
 
 class IsAdmin(permissions.BasePermission):
     """
-    Permiso para usuarios del grupo 'Administradores'.
+    Permiso para usuarios del grupo 'Administradores' o que sean staff.
     """
+    message = "No tienes permiso para realizar esta acción."
 
-    message = "No tienes permiso para realizar esta acción."  # Personalizar mensaje
-    
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.groups.filter(name="Administradores").exists()
+        return (
+            request.user.is_authenticated and 
+            (request.user.is_staff or request.user.groups.filter(name="Administradores").exists())
+        )
 
 
 class IsATC(permissions.BasePermission):
     """
-    Permiso para usuarios del grupo 'ATC'.
+    Permiso para usuarios del grupo 'ATC' o que sean staff.
     """
-
-    message = "No tienes permiso para realizar esta acción."  # Personalizar mensaje
+    message = "No tienes permiso para realizar esta acción."
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.groups.filter(name="ATC").exists()
+        return (
+            request.user.is_authenticated and 
+            (request.user.is_staff or request.user.groups.filter(name="ATC").exists())
+        )
 
 
 class IsOperador(permissions.BasePermission):
     """
-    Permiso para usuarios del grupo 'Operadores'.
+    Permiso para usuarios del grupo 'Operadores' o que sean staff.
     """
-
-    message = "No tienes permiso para realizar esta acción."  # Personalizar mensaje
+    message = "No tienes permiso para realizar esta acción."
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.groups.filter(name="Operadores").exists()
+        return (
+            request.user.is_authenticated and 
+            (request.user.is_staff or request.user.groups.filter(name="Operadores").exists())
+        )
 
 
 class IsRetenciones(permissions.BasePermission):
     """
-    Permiso para usuarios del grupo 'Retenciones'.
+    Permiso para usuarios del grupo 'Retenciones' o que sean staff.
     """
+    message = "No tienes permiso para realizar esta acción."
 
-    message = "No tienes permiso para realizar esta acción."  # Personalizar mensaje
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.groups.filter(name="Retenciones").exists()
+        return (
+            request.user.is_authenticated and 
+            (request.user.is_staff or request.user.groups.filter(name="Retenciones").exists())
+        )
